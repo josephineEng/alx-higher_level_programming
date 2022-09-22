4b9ada564818184c811f403e930b6631a60e32b1
JosephineBonka <josephinebonka123@gmail.com>
2022-09-23 01:01:26 +0300

    new

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fun = 'C is fun';
+const thon = 'Python is cool';
+const javas = 'JavaScript is amazing';
+console.log(fun);
+console.log(thon);
+console.log(javas);
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+    defining a rectangle class with the propeties
+    height and width
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
